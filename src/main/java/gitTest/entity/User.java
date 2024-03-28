012f6c3d999459fc8e18931fa359bd302c43e7c4
XinDeng <d3055592816@163.com>
2024-03-28 16:31:11 +0800

    dev

--- src/main/java/gitTest/entity/User.java
+++ src/main/java/gitTest/entity/User.java
@@ -2,6 +2,8 @@ package gitTest.entity;
 
 public class User {
 	public static void main(String[] args) {
-		System.out.println("2");
+		System.out.println("3");
+		System.out.println("dev");
+		
 	}
 }
